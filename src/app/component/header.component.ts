8ba38032494a049e1ac0288a1a51fc3d9c9f8bd7
Aditya Annamreddi <Aditya_Annamreddi@Progressive.com>
2023-07-17 15:54:49 -0400

    first commit

---
+++ src/app/component/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+standalone: true,
+selector: 'app-header',
+template: `
+  <h1> Intro to Programing</h1>
+  <p> Sample Full-Stack Application</p>
+`
+})
+
+export class HeaderComponent{
+
+}
